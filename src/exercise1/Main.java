78ea7a11bb90dbc2d4349bcdcc25601d81dfbc33
leilaweicman <leilaweic@hotmail.com>
2025-09-30 10:27:14 +0200

    test(exercise1): add Main class to demonstrate NoGenericMethods usage

---
+++ src/exercise1/Main.java
@@ -0,0 +1,14 @@
+package exercise1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        NoGenericMethods obj1 = new NoGenericMethods("A", "B", "C");
+        NoGenericMethods obj2 = new NoGenericMethods("C", "A", "B");
+        NoGenericMethods obj3 = new NoGenericMethods("B", "C", "A");
+
+        System.out.println(obj1);
+        System.out.println(obj2);
+        System.out.println(obj3);
+    }
+}
