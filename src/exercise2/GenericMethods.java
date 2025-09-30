db3ca832377d8884122448ccabd5c87257f0fad3
leilaweicman <leilaweic@hotmail.com>
2025-09-30 13:09:02 +0200

    feat(exercise2): add GenericMethods class with generic printArguments method

---
+++ src/exercise2/GenericMethods.java
@@ -0,0 +1,10 @@
+package exercise2;
+
+public class GenericMethods {
+
+    public <T, U, V> void printArguments(T arg1, U arg2, V arg3) {
+        System.out.println("Argument 1: " + arg1);
+        System.out.println("Argument 2: " + arg2);
+        System.out.println("Argument 3: " + arg3);
+    }
+}
